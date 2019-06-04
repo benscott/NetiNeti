9ddba97ecb3775799799683ec53c9da3e7645411
benscott.co.uk <ben@benscott.co.uk>
2019-06-04 10:37:33 +0100

    Fix training file encoding

--- src/neti_neti_trainer.py
+++ src/neti_neti_trainer.py
@@ -16,16 +16,18 @@ import random
 import os
 from neti_neti_helper import strip_token, get_words_slice
 
+
 class NetiNetiTrainer:
     """A class that defines the training algorithm and the training files
     and actually trains a natrual language toolkit object (nltk)
     """
-    def __init__(self, positive_training_file = "data/names_in_context.txt",
-                 negative_training_file = "data/no_names.txt",
-                 sci_names_file = "data/white_list.txt",
+
+    def __init__(self, positive_training_file="data/names_in_context.txt",
+                 negative_training_file="data/no_names.txt",
+                 sci_names_file="data/white_list.txt",
                  learning_algorithm="NB",
-                 sci_names_training_num = 10000,
-                 negative_trigrams_num = 5000, context_span=1):
+                 sci_names_training_num=10000,
+                 negative_trigrams_num=5000, context_span=1):
         """Builds and trains the NetiNeti model
 
         Keyword arguments:
@@ -45,14 +47,17 @@ class NetiNetiTrainer:
             for context training (default 1)
 
         """
-        self._positive_training_file = os.path.dirname(os.path.realpath(__file__)) + "/" + positive_training_file
-        self._negative_training_file = os.path.dirname(os.path.realpath(__file__)) + "/" + negative_training_file
-        self._sci_names_file = os.path.dirname(os.path.realpath(__file__)) + "/" + sci_names_file
+        self._positive_training_file = os.path.dirname(
+            os.path.realpath(__file__)) + "/" + positive_training_file
+        self._negative_training_file = os.path.dirname(
+            os.path.realpath(__file__)) + "/" + negative_training_file
+        self._sci_names_file = os.path.dirname(
+            os.path.realpath(__file__)) + "/" + sci_names_file
         self._sci_names_training_num = sci_names_training_num
         self._negative_trigrams_num = negative_trigrams_num
         self._context_span = context_span
         self.learning_algorithm = learning_algorithm
-        self._model = None # model is added during the training
+        self._model = None  # model is added during the training
 
         self._sci_names, self._white_list = self._tokenize_sci_names()
         featuresets = self._get_training_data()
@@ -76,7 +81,7 @@ class NetiNetiTrainer:
         sci_names = all_sci_names[:self._sci_names_training_num]
         white_list = {}
         for sci_name in all_sci_names:
-            #TODO after refactoring: change to split() to avoid empty token.
+            # TODO after refactoring: change to split() to avoid empty token.
             # Then figure out how to eliminate empty token from other places
             words = sci_name.split(" ")
             for word in words:
@@ -100,7 +105,7 @@ class NetiNetiTrainer:
 
         """
         featuresets = []
-        ndata = open(self._negative_training_file).read()
+        ndata = open(self._negative_training_file).read().decode('utf8')
         ntokens = nltk.word_tokenize(ndata)
         neg_trigrams = nltk.trigrams(ntokens)
         index = -1
@@ -112,26 +117,26 @@ class NetiNetiTrainer:
             trigram = first_word + " " + second_word + " " + third_word
             bigram = first_word + " " + second_word
             if (first_word[0].isupper() and first_word[1:].islower()
-                and second_word.islower()):
+                    and second_word.islower()):
 
                 featuresets.append((self.taxon_features(bigram, ntokens,
-                    index, 1), 'not-a-taxon'))
+                                                        index, 1), 'not-a-taxon'))
                 featuresets.append((self.taxon_features(first_word, ntokens,
-                    index, 0), 'not-a-taxon'))
+                                                        index, 0), 'not-a-taxon'))
                 # TODO find out if it makes sense to move it as a separate
                 # if statement
                 if(third_word.islower()):
                     trigram_count += 1
                     featuresets.append((self.taxon_features(trigram, ntokens,
-                                        index, 2), 'not-a-taxon'))
-            #TODO after refactoring: it might make sense to generate unigram
+                                                            index, 2), 'not-a-taxon'))
+            # TODO after refactoring: it might make sense to generate unigram
             # once, looks like we have duplicated unigrams here
             if(second_word[0].isupper() and second_word[1:].islower()):
                 featuresets.append((self.taxon_features(second_word, ntokens,
-                    index + 1, 0), 'not-a-taxon'))
+                                                        index + 1, 0), 'not-a-taxon'))
             if(third_word[0].isupper() and third_word[1:].islower()):
                 featuresets.append((self.taxon_features(third_word, ntokens,
-                    index + 2, 0), 'not-a-taxon'))
+                                                        index + 2, 0), 'not-a-taxon'))
         random.shuffle(featuresets)
         return(featuresets)
 
@@ -141,10 +146,10 @@ class NetiNetiTrainer:
         for line in open(self._positive_training_file):
             if line.strip():
                 name, context = line.split("---", 1)
-            data.append({ 'name' : name.strip(), 'context' : context.strip() })
+            data.append({'name': name.strip(), 'context': context.strip()})
         for sci_name in self._sci_names:
             if sci_name:
-                data.append({ 'name' : sci_name, 'context' : sci_name })
+                data.append({'name': sci_name, 'context': sci_name})
         return data
 
     def _get_positive_featuresets(self, positive_training_data):
@@ -152,14 +157,14 @@ class NetiNetiTrainer:
         featuresets = []
         for data in positive_training_data:
             name = data['name']
-            context = data['context']
+            context = data['context'].decode('utf8')
             context_tokens = nltk.word_tokenize(context)
             name_tokens = nltk.word_tokenize(name)
             try:
                 index = context_tokens.index(name_tokens[0])
             except ValueError:
                 index = 0
-                #TODO after refactoring: should index be 0 or
+                # TODO after refactoring: should index be 0 or
                 # it should be -1 or something?
             span = len(name_tokens) - 1
 
@@ -167,11 +172,11 @@ class NetiNetiTrainer:
             featuresets.append((features, 'taxon'))
 
             # create abbreviated version of the name (e.g. Aus bus -> A. bus)
-            #and make features.
+            # and make features.
             if(len(name_tokens[0]) > 1 and name_tokens[0][1] != "."):
                 abbr_name = name_tokens[0][0]+". " + " ".join(name_tokens[1:])
                 features = self.taxon_features(abbr_name,
-                    context_tokens, index,span)
+                                               context_tokens, index, span)
                 featuresets.append((features, 'taxon'))
         return featuresets
 
@@ -189,31 +194,31 @@ class NetiNetiTrainer:
         context_span = self._context_span
         features = {}
         vowels = ['a', 'e', 'i', 'o', 'u']
-        last_chars = ['a', 'i', 's', 'm'] #last letter (LL) weight
+        last_chars = ['a', 'i', 's', 'm']  # last letter (LL) weight
         last_chars_reduced = ['e', 'o']   # Reduced LL weight
         last_chars_all = last_chars + last_chars_reduced
         penultimate_chars = ['i', 'u']    # penultimate L weight
         string_weight = 0
         weight_increment = 5
 
-        features['last3_first_word']  = get_words_slice(words, 0, -3, 1000)
+        features['last3_first_word'] = get_words_slice(words, 0, -3, 1000)
         features['last3_second_word'] = get_words_slice(words, 1, -3, 1000)
-        features['last3_third_word']  = get_words_slice(words, 2, -3, 1000)
-        features['last2_first_word']  = get_words_slice(words, 0, -2, 1000)
+        features['last3_third_word'] = get_words_slice(words, 2, -3, 1000)
+        features['last2_first_word'] = get_words_slice(words, 0, -2, 1000)
         features['last2_second_word'] = get_words_slice(words, 1, -2, 1000)
-        features['last2_third_word']  = get_words_slice(words, 2, -2, 1000)
-        features['char1_first_word']  = get_words_slice(words, 0, 0)
+        features['last2_third_word'] = get_words_slice(words, 2, -2, 1000)
+        features['char1_first_word'] = get_words_slice(words, 0, 0)
         features['char-1_first_word'] = get_words_slice(words, 0, -1)
-        features['char2_first_word']  = get_words_slice(words, 0, 1)
+        features['char2_first_word'] = get_words_slice(words, 0, 1)
         features['char-2_first_word'] = get_words_slice(words, 0, -2)
 
         features["char-1_first_word_in_lc"] = (features['char-1_first_word']
-                in last_chars)
+                                               in last_chars)
         if features["char-1_first_word_in_lc"]:
             string_weight += weight_increment
 
         features["char-1_first_word_in_lcr"] = (features['char-1_first_word']
-                in last_chars_reduced)
+                                                in last_chars_reduced)
         if features["char-1_first_word_in_lcr"]:
             string_weight += weight_increment - 3
 
@@ -224,64 +229,66 @@ class NetiNetiTrainer:
             string_weight += weight_increment
 
         features["char-1_second_word_in_lcr"] = (char_last_second
-                in last_chars_reduced)
+                                                 in last_chars_reduced)
         if features["char-1_second_word_in_lcr"]:
             string_weight += weight_increment - 3
 
         features["char-1_third_word_in_lca"] = (get_words_slice(words, 2, -1)
-                in last_chars_all)
+                                                in last_chars_all)
         if features["char-1_third_word_in_lca"]:
             string_weight += weight_increment - 2
 
         features["char-2_third_word_in_lca"] = (get_words_slice(words, 2, -2)
-                in last_chars_all)
+                                                in last_chars_all)
 
         features["char-1_first_word_in_vwl"] = (features['char-1_first_word']
-                in vowels)
-        features["char-2_first_word_in_lc"]  = (features['char-2_first_word']
-                in last_chars)
+                                                in vowels)
+        features["char-2_first_word_in_lc"] = (features['char-2_first_word']
+                                               in last_chars)
         features["char-2_first_word_in_lcr"] = (features['char-2_first_word']
-                in last_chars_reduced)
-        features["char-2_first_word_in_pc"]  = (features['char-2_first_word']
-                in penultimate_chars)
+                                                in last_chars_reduced)
+        features["char-2_first_word_in_pc"] = (features['char-2_first_word']
+                                               in penultimate_chars)
         char_before_last_second = get_words_slice(words, 1, -2)
 
         # TODO BUG!!!! remove after refactoring
         # features["char-2_first_word_in_lc"]  = char_before_last_second
         # end BUG
 
-        features["char-2_second_word_in_lc"]  = (char_before_last_second
-                in last_chars)
+        features["char-2_second_word_in_lc"] = (char_before_last_second
+                                                in last_chars)
         features["char-2_second_word_in_lcr"] = (char_before_last_second
-                in last_chars_reduced)
-        features["char-2_second_word_in_pc"]  = (char_before_last_second
-                in penultimate_chars)
+                                                 in last_chars_reduced)
+        features["char-2_second_word_in_pc"] = (char_before_last_second
+                                                in penultimate_chars)
 
         # TODO BUG!!!! remove after refactoring
         # features["char-2_first_word_in_pc"] = char_before_last_second
         # end BUG
 
-        features["first_word_in_wl"]  = \
+        features["first_word_in_wl"] = \
             self._white_list.has_key(words[0].lower())
         features["second_word_in_wl"] = \
-          self._white_list.has_key(get_words_slice(words, 1, 0, 1000).lower())
-        features["third_word_in_wl"]  = \
-          self._white_list.has_key(get_words_slice(words, 2, 0, 1000).lower())
+            self._white_list.has_key(
+                get_words_slice(words, 1, 0, 1000).lower())
+        features["third_word_in_wl"] = \
+            self._white_list.has_key(
+                get_words_slice(words, 2, 0, 1000).lower())
         for i in range(context_span):
             features[str(i + 1) + "_context"] = \
                 strip_token(get_words_slice(context_array,
-                    index + span + i + 1, 0, 1000))
+                                            index + span + i + 1, 0, 1000))
             if(index + i - context_span < 0):
                 features[str(i - context_span) + "_context"] = 'Null'
             else:
                 features[str(i - context_span) + "_context"] = \
                     strip_token(get_words_slice(context_array,
-                        index + i - context_span, 0, 1000))
+                                                index + i - context_span, 0, 1000))
         try:
             features["1up_2dot_rest_lower_case"] = (token[0].isupper() and
-                                            token[1] is "." and
-                                            token[2] is " " and
-                                            token[3:].islower())
+                                                    token[1] is "." and
+                                                    token[2] is " " and
+                                                    token[3:].islower())
         except IndexError:
             features["1up_2dot_rest_lower_case"] = False
         features["token"] = token
@@ -315,7 +322,7 @@ class NetiNetiTrainer:
             model = nltk.NaiveBayesClassifier.train(featuresets)
         elif(self.learning_algorithm == "MaxEnt"):
             model = nltk.MaxentClassifier.train(featuresets, "MEGAM",
-                                                 max_iter=15)
+                                                max_iter=15)
         elif(self.learning_algorithm == "DecisionTree"):
             model = nltk.DecisionTreeClassifier.train(featuresets, 0.05)
         self._model = model
@@ -323,4 +330,3 @@ class NetiNetiTrainer:
     def get_model(self):
         """An accessor method for the model."""
         return self._model
-
